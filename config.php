2c4c6a01b51b09ab817b728174c778c1cfe8b6ff
Your Name <you@example.com>
2022-07-31 14:50:37 +0530

    Food data for daily diary

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$GLOBALS['main_user_location']='/var/gmcs_config/staff.conf';
+$GLOBALS['user_database']='food';
+$GLOBALS['user_table']='user';
+$GLOBALS['user_id']='id';
+$GLOBALS['user_pass']='password';
+$GLOBALS['expiry_period']='+ 6 months';
+$GLOBALS['expirydate_field']='expirydate';
+$GLOBALS['application_name']='Food Dairy';
+
+$GLOBALS['database']='food';
+$GLOBALS['record_tables']='record_tables';
+$GLOBALS['all_records_limit']=100;
+?>
